9c8efbe6021eed86b27392548df08713e2799d2a
Diogo <51659633+diogosvicente@users.noreply.github.com>
2023-05-05 17:47:16 -0300

    estava na uerj

--- ex01/Phonebook.hpp
+++ ex01/Phonebook.hpp
@@ -6,7 +6,7 @@
 /*   By: dioda-si <dioda-si@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/30 12:30:04 by dioda-si          #+#    #+#             */
-/*   Updated: 2023/05/01 23:20:28 by dioda-si         ###   ########.fr       */
+/*   Updated: 2023/05/04 19:42:39 by dioda-si         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,13 @@
 # define PHONEBOOK_HPP
 
 # include "Contact.hpp"
+
+// Constantes para as cores
+const std::string RESET = "\033[0m";
+const std::string ULINE = "\033[4m";
+const std::string RED = "\033[31m";
+const std::string GREEN = "\033[32m";
+const std::string YELLOW = "\033[33m";
 
 class Phonebook
 {
@@ -24,9 +31,12 @@ class Phonebook
 		Phonebook(void);
 		~Phonebook(void);
 		std::string	requiredField(std::string label);
-		void	add(void);
-		void	search(char *s);
-		void	exit(int option);
+		void 		saveContact(std::string firstName, \
+					std::string lastName, std::string nickname, \
+					std::string phoneNumber, std::string darkestSecret);
+		void		add(void);
+		void		details(int id);
+		void		search(void);
 };
 
 #endif
