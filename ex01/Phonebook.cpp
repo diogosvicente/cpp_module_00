3e51902ed51d2cbd6e07a6b9178ae527022a5633
diogosvicente <diogosk84ever@hotmail.com>
2023-05-06 16:23:13 -0300

    first release

--- ex01/Phonebook.cpp
+++ ex01/Phonebook.cpp
@@ -6,7 +6,7 @@
 /*   By: dioda-si <dioda-si@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/30 14:12:31 by dioda-si          #+#    #+#             */
-/*   Updated: 2023/05/06 14:15:25 by dioda-si         ###   ########.fr       */
+/*   Updated: 2023/05/06 16:22:02 by dioda-si         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,7 +55,7 @@ void	Phonebook::add(void)
 	std::string	darkestSecret;
 	int			flag = 0;
 
-	if (this->_index == 2)
+	if (this->_index == 8)
 	{
 		this->_index = 0;
 		flag = 1;
@@ -95,30 +95,49 @@ void	Phonebook::saveContact(std::string firstName, std::string lastName, std::st
 
 void	Phonebook::search()
 {
+	std::string	id;
 	if (this->_phonebook[0].getFirstName().empty())
 	{
 		std::cout << RED << "The phonetbook is empty!" << RESET << std::endl;
 		return;
 	}
-	std::cout << " ___________________________________________ " << std::endl;
-	std::cout << "|     Index|First Name| Last Name|  Nickname|" << std::endl;
-	std::cout << "|----------|----------|----------|----------|" << std::endl;
+	std::cout << YELLOW << " ___________________________________________ " << RESET << std::endl;
+	std::cout << YELLOW <<  "|     Index|First Name| Last Name|  Nickname|" << RESET <<std::endl;
+	std::cout << YELLOW << "|----------|----------|----------|----------|" << RESET << std::endl;
 	for (int i = 0; i < 8; i++)
 	{
 		if (this->_phonebook[i].getFirstName().empty())
 			break;
-		std::cout << "|         " << i << "|";
+		std::cout << YELLOW << "|         " << RESET \
+			<< i+1 << YELLOW << "|" << RESET;
 		align(this->_phonebook[i].getFirstName());
-		std::cout << "|";
+		std::cout << YELLOW << "|" << RESET;
 		align(this->_phonebook[i].getLastName());
-		std::cout << "|";
+		std::cout << YELLOW << "|" << RESET;
 		align(this->_phonebook[i].getNickname());
-		std::cout << "|";
+		std::cout << YELLOW << "|" << RESET;
 		std::cout << std::endl;
+	}
+	std::cout << YELLOW << " ------------------------------------------- " << RESET << std::endl;
+	
+	while(!std::cin.eof())
+	{
+		std::cout << std::endl << "Type a contact index to see full information: ";
+		std::getline (std::cin, id);
+		if (!id.empty())
+		{
+			if (id.size() == 1 && id[0] > '0' && id[0] < '9')
+			{
+				if (!details(id[0]-48))
+					break;
+			}
+			else if (!id.empty() && (((id[0] <= '0' || id[0] >= '9')) || id.size() > 1))
+				std::cout << RED << "Invalid index" << RESET << std::endl;
+		}
 	}
 }
 
-void	align(std::string content)
+void	Phonebook::align(std::string content)
 {
 	int	size;
 	int	i;
@@ -133,13 +152,21 @@ void	align(std::string content)
 		std::cout << std::setw(10) << content;
 }
 
-void	Phonebook::details(int id)
+int	Phonebook::details(int id)
 {
-	std::cout << std::endl << ULINE << "Contact details:" << RESET << std::endl;
-	std::cout << "First Name: " << this->_phonebook[id].getFirstName() << std::endl;
-	std::cout << "Last Name: " << this->_phonebook[id].getLastName() << std::endl;
-	std::cout << "Nickname: " << this->_phonebook[id].getNickname() << std::endl;
-	std::cout << "Phone Number: " << this->_phonebook[id].getPhoneNumber() << std::endl;
+	id -=1;
+	if (this->_phonebook[id].getFirstName().empty())
+	{
+		std::cout << RED << \
+			"There's no contact added at the [" << id+1 << "] index! " << RESET << std::endl;
+		return(1);
+	}
+	std::cout << std::endl << ULINE << GREEN << "Contact details:" << RESET << std::endl;
+	std::cout << "First Name: \t" << this->_phonebook[id].getFirstName() << std::endl;
+	std::cout << "Last Name: \t" << this->_phonebook[id].getLastName() << std::endl;
+	std::cout << "Nickname: \t" << this->_phonebook[id].getNickname() << std::endl;
+	std::cout << "Phone Number: \t" << this->_phonebook[id].getPhoneNumber() << std::endl;
 	std::cout << "Darkest Secret: " << this->_phonebook[id].getDarkestSecret() << std::endl;
+	return(0);
 }
 
--- ex01/Phonebook.hpp
+++ ex01/Phonebook.hpp
@@ -6,7 +6,7 @@
 /*   By: dioda-si <dioda-si@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/30 12:30:04 by dioda-si          #+#    #+#             */
-/*   Updated: 2023/05/04 19:42:39 by dioda-si         ###   ########.fr       */
+/*   Updated: 2023/05/06 15:46:43 by dioda-si         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,6 @@
 
 # include "Contact.hpp"
 
-// Constantes para as cores
 const std::string RESET = "\033[0m";
 const std::string ULINE = "\033[4m";
 const std::string RED = "\033[31m";
@@ -34,8 +33,9 @@ class Phonebook
 		void 		saveContact(std::string firstName, \
 					std::string lastName, std::string nickname, \
 					std::string phoneNumber, std::string darkestSecret);
+		void		align(std::string content);
+		int		details(int id);
 		void		add(void);
-		void		details(int id);
 		void		search(void);
 };
 
